b1ad36d2d3968054db1d39fef10750741049bcef
leandro <alvim.lgm@gmail.com>
2025-03-11 13:11:25 -0300

    fix: 1 label ->1 user

--- apps/web/app/labeler/models/label.ts
+++ apps/web/app/labeler/models/label.ts
@@ -1,7 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
-import type { HasMany } from '@adonisjs/lucid/types/relations'
-
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 import User from '#users/models/user'
 
 export default class Label extends BaseModel {
@@ -26,7 +25,7 @@ export default class Label extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
 
-  // Relação com usuário
-  @hasMany(() => User)
-  declare user: HasMany<typeof User>
+  //Um Label pertence a um User
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
 }
