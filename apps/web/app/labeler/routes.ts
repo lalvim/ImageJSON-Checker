e608cb8cc6d5fbc7cf0212ee7f95f618af47fa79
leandro <alvim.lgm@gmail.com>
2025-03-13 12:38:17 -0300

    refact: reorganization

--- apps/web/app/labeler/routes.ts
+++ apps/web/app/labeler/routes.ts
@@ -3,8 +3,17 @@ import { middleware } from '#start/kernel'
 
 const LabelingController = () => import('#labeler/controllers/labeling_controller')
 
-router.group(() => {
-  router.get('/labels', [LabelingController, 'index']).as('labels.index')
-  router.get('/label/:filename', [LabelingController, 'show']).as('labels.show')
-  router.post('/labels', [LabelingController, 'store']).as('labels.store')
-}).middleware(middleware.auth())
+router
+  .get('/labels', [LabelingController, 'index'])
+  .middleware(middleware.auth())
+  .as('labels.index')
+
+router
+  .get('/label/:filename', [LabelingController, 'show'])
+  .middleware(middleware.auth())
+  .as('labels.show')
+
+router
+  .post('/labels', [LabelingController, 'store'])
+  .middleware(middleware.auth())
+  .as('labels.store')
