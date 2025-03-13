8e2110e4b4b92993db052861025ec0c280a2014d
leandro <alvim.lgm@gmail.com>
2025-03-13 12:39:29 -0300

    fix: migration

--- apps/web/app/labeler/database/migrations/1741828422522_create_labels_table.ts
+++ apps/web/app/labeler/database/migrations/1741828422522_create_labels_table.ts
@@ -3,24 +3,20 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 export default class Labels extends BaseSchema {
   protected tableName = 'labels'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('users.id')
-        .onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.string('file_name').notNullable()
       table.boolean('is_correct').defaultTo(false)
       table.text('observation').nullable()
 
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      // Cria as colunas created_at e updated_at automaticamente
+      table.timestamps(true)
     })
   }
-  
-  async down() {
+
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
